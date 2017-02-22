943757ff2f53c3ed39588eb0f5b9f29e99b7f76a
zjuchenyuan <chenyuan@zju.edu.cn>
2017-02-22 17:08:45 +0800

    gh: change compile command to fix display bug

--- compile.sh
+++ compile.sh
@@ -17,5 +17,9 @@ for filename in os.listdir("."):
         data = replace_css.sub('assets/css/style.css"',data)
         if filename != "index.html":
             data = data.replace("View on GitHub</a>","""View on GitHub</a>&nbsp;<a href="http://py3.io" class="btn btn-back"><span class="icon"></span>Back to Index</a>""")
+        if filename == "Python.html":
+            data = data.replace('<span class="err">','<span class="s">')
+        elif filename == "Docker.html":
+            data = data.replace("--format '' 容器名称","--format '{{.NetworkSettings.IPAddress}}' 容器名称")
         open(filename,"w",encoding='utf-8').write(data)
 PYTHON
