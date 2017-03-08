005d48295f1293e133a81f46312b395850f0070e
zjuchenyuan <chenyuan@zju.edu.cn>
2017-03-08 10:37:22 +0800

    add 公安备案

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@
 rm *.html
 bundle exec jekyll build -d docs --no-watch -I
 cp docs/*.html ./
-echo "<hr><small>Last updated: `date --iso-8601=seconds`</small>&nbsp;&nbsp;&nbsp;<a href="http://www.miitbeian.gov.cn/">浙ICP备15043819号-2</a>" >> index.html
+echo "<hr><small>Last updated: `date --iso-8601=seconds`&nbsp;&nbsp;&nbsp;<a href=http://www.miitbeian.gov.cn/>浙ICP备15043819号-2</a>&nbsp;&nbsp;<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602007826"><img src="beian.png" height="14px">浙公网安备 33010602007826号</p></a></small>" >> index.html
 cp code/randomstring.html ./p.html
 cp doc/python/quickstart.html ./
 python3 << PYTHON
