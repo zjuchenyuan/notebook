236089c2d127db45f63d67a85ffeb36af1138bb3
zjuchenyuan <chenyuan@zju.edu.cn>
2017-03-26 11:41:23 +0800

    minor modification for code

--- code/spider.oncokb.js
+++ code/spider.oncokb.js
@@ -9,28 +9,28 @@ page.viewportSize = { width: 1920, height: 1080 };
 url="http://oncokb.org/#/gene/"+NAME;
 
 function capture(){
-	console.log("save...");
-	page.render('output_'+NAME+'.pdf',{format: 'pdf', quality: '100'});
+    console.log("save...");
+    page.render('output_'+NAME+'.pdf',{format: 'pdf', quality: '100'});
     fp.write(page.content);
     fp.close();
-	phantom.exit();
+    phantom.exit();
 }
 function timeout(){
-	console.log("Timeout!");
-	var flog=fs.open("tiemout.log","a");
-	flog.write(NAME);
-	flog.close();
+    console.log("Timeout!");
+    var flog=fs.open("tiemout.log","a");
+    flog.write(NAME);
+    flog.close();
 }
 page.onResourceReceived = function(response) {
-  //console.log('Receive ' + JSON.stringify(response, undefined, 4));
-  //console.log(response.url);
-  if(response.url.indexOf("http")==0){
-	  console.log(response.url);
-  }
-  if(response.url.indexOf("http://www.cbioportal.org/api-legacy/studies")!=-1){
-  	console.log("ok...");
-  	setTimeout(capture,5000);
-  }
+    //console.log('Receive ' + JSON.stringify(response, undefined, 4));
+    //console.log(response.url);
+    if(response.url.indexOf("http")==0){
+        console.log(response.url);
+    }
+    if(response.url.indexOf("http://www.cbioportal.org/api-legacy/studies")!=-1){
+        console.log("ok...");
+        setTimeout(capture,5000);
+    }
 };
 page.open(url);
 setTimeout(timeout,120000);
