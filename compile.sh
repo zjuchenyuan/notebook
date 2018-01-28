7a9066efc6f338e42f91c4712cf636de45cab48c
zjuchenyuan <chenyuan@zju.edu.cn>
2018-01-28 23:10:16 +0800

    [beian] add beian.png

--- compile.sh
+++ compile.sh
@@ -8,4 +8,3 @@ popd
 python3 code/upyun_purge.py &
 
 mkdocs build
-echo "<hr><small>Last updated: `date --iso-8601=seconds`&nbsp;&nbsp;&nbsp;<a href=http://www.miitbeian.gov.cn/>浙ICP备15043819号-2</a>&nbsp;&nbsp;<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=33010602007826"><img src="beian.png" height="14px">浙公网安备 33010602007826号</p></a></small>" >> index.html
