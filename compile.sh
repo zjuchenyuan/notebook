2ac10e4972ef4b9626237a99f3b673e0fe064d6a
zjuchenyuan <chenyuan@zju.edu.cn>
2020-05-29 11:42:23 +0800

    retry

--- compile.sh
+++ compile.sh
@@ -10,4 +10,5 @@ python3 code/upyun_purge.py &
 mkdocs build
 cd docs
 sed -i 's#https://fonts.googleapis.com/css#/assets/css/fonts.css#g' $(find -type f -name "*.html")
+cd ..
 cp assets/css/main.4b9ffd7b.min.css docs/assets/stylesheets/
