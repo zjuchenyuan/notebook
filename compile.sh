1793a13b16001ee8953732fb8d19af3ed84b6e10
zjuchenyuan <chenyuan@zju.edu.cn>
2020-08-04 23:44:59 +0800

    [compile] update compile script: refresh btc.html and blog.chenyuan.me

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@
 ## 刷新又拍云缓存，代码在https://github.com/zjuchenyuan/EasyLogin/tree/master/examples/upyun
 pushd /mnt/d/Seafile/Developer/EasyLogin/examples/upyun/
 python3 upyun.py 'https://py3.io/@' &
+python3 upyun.py 'https://blog.chenyuan.me/@' &
 popd
 python3 code/upyun_purge.py &
 for i in *.md; do ln -f $i mdfiles/$i; done
@@ -12,4 +13,4 @@ cd docs
 sed -i 's#https://fonts.googleapis.com/css#/assets/css/fonts.css#g' $(find -type f -name "*.html")
 cd ..
 cp assets/css/main.4b9ffd7b.min.css docs/assets/stylesheets/
-sed -i 's#https://d.py3.io/btc.html#<div id="realtimeprofit"></div><script>fetch("https://d.py3.io/btc.html").then(r=>r.text()).then(html=>document.getElementById("realtimeprofit").innerHTML=html)</script>#' docs/Bitcoin/index.html
+sed -i 's#https://d.py3.io/btc.html#<div id="realtimeprofit"></div><script>fetch("https://d.py3.io/btc.html?_t="+new Date().getTime()).then(r=>r.text()).then(html=>document.getElementById("realtimeprofit").innerHTML=html)</script>#' docs/Bitcoin/index.html
